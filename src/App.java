279e1731f78643419591ba85887dd2b88229479e
EstebanBustamante <96210984+EstebanBustamante@users.noreply.github.com>
2023-06-12 15:51:16 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Programa que le tiras la fecha de un parcial y cuantas horas estudias por semana y te tire cuantas horas te quedan de estudio");
+    }
+}
