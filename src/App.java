4af65666beaa87c3647039bb93a7dde06c95aa27
EstebanBustamante <96210984+EstebanBustamante@users.noreply.github.com>
2023-06-12 21:38:24 -0300

    cambios

--- src/App.java
+++ src/App.java
@@ -1,5 +1,41 @@
+import java.time.temporal.ChronoUnit;
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Programa que le tiras la fecha de un parcial y cuantas horas estudias por semana y te tire cuantas horas te quedan de estudio");
+        Scanner scanner = new Scanner(System.in);
+        LocalDate diaDeHoy = LocalDate.now();
+        System.out.print("Ingrese el numero de examenes pendientes: ");
+        int numeroExamenes = scanner.nextInt();
+        scanner.nextLine();
+
+        LocalDate[] fechasParciales = new LocalDate[numeroExamenes];
+        String[] nombreMateria = new String[numeroExamenes];
+        int[] tiempoRestante = new int[numeroExamenes];
+
+         for (int i = 0; i < numeroExamenes; i++) {
+             System.out.print("Ingrese la fecha del examen numero " + (i + 1) + " (a\u00F1o-mes-dia): ");
+             String diaDelExamen = scanner.nextLine();
+             fechasParciales[i] = LocalDate.parse(diaDelExamen);
+ 
+             System.out.print("Ingrese el nombre de la materia del examen numero " + (i + 1) + ": ");
+             nombreMateria[i] = scanner.nextLine();
+         }
+ 
+         System.out.print("Ingrese la cantidad de horas promedio que estudia por dia: ");
+         int horasDeEstudioDiarias = scanner.nextInt();
+ 
+         for (int i = 0; i < numeroExamenes; i++) {
+             long remainingDays = ChronoUnit.DAYS.between(diaDeHoy, fechasParciales[i]);
+             tiempoRestante[i] = ((int) remainingDays * horasDeEstudioDiarias);
+         }
+ 
+         for (int i = 0; i < numeroExamenes; i++) {
+             System.out.println("Para el examen de " + nombreMateria[i] + ":");
+             System.out.println("Tenes " + tiempoRestante[i] + " horas restantes para estudiar.");
+         }
+
+         scanner.close();
     }
 }
